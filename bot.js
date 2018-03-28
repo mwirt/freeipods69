322efdad1e06f2ad3e71468572a2aa7b4581b2ce
Max Wirt <max@sincerely.tv>
2018-03-28 19:46:53 -0400

    set up keys

--- bot.js
+++ bot.js
@@ -2,10 +2,26 @@ console.log('The bot is starting')
 
 var Twit = require('twit');
 
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+var config = require('./config');
+
+var T = new Twit(config);
+
+var params = {
+	q: '"richard album"',
+	count: 10,
+	tweet_mode: 'extended'
+};
+
+T.post('statuses/update', { status: 'hot new site: http://freeipods69.party #freeipods #69 #party #twitterbot' }, function(err, data, response) {
+  console.log(data)
 })
+
+function gotData(err, data, response) {
+	var tweets = data.statuses;
+	for (var i = 0; i < tweets.length; i++) {
+		console.log(tweets[i].full_text)
+		console.log("")
+		console.log("")
+		console.log("")
+	}
+};
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	consumer_key:         'l5ImviGa8S7uB6eQ7DnI5Nyhp',
+    consumer_secret:      'fTxgTzAQSEB4pSGRg9Y6hupDE2tmbds4eOZMSXhZS27fODdSsp',
+    access_token:         '890241306672459780-Zfs57Oqcu8EJTrd2ugqleNdStvVdNdK',
+    access_token_secret:  'IsLc2waCROJU1QUMnjOsuY6mm4GrCfBuvWin1DCu9dyUO',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
